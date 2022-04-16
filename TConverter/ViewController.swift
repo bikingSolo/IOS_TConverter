900564b74cfc2e373e790a69ac087f652bd7cc43
Leo <borisowsky.leff@yandex.ru>
2022-04-16 17:10:22 +0300

    Initial Commit

---
+++ TConverter/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TConverter
+//
+//  Created by Лев on 22.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var celcium: UILabel!
+    
+    @IBOutlet weak var farengeit: UILabel!
+    
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    @IBAction func temp(_ sender: UISlider) {
+    }
+    
+}
+
